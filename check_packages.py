f0df605a09a3d57ab0e0f0b3a4ab2c1fbd98d862
Mauro Baladés <mauro.balades@tutanota.com>
2024-01-06 22:42:51 +0000

    Added versioning system and scans

--- check_packages.py
+++ check_packages.py
@@ -5,6 +5,10 @@ import tomllib
 
 has_error = False
 
+GREEN = "\033[92m"
+RED = "\033[91m"
+RESET = "\033[0m"
+
 # Iterate all packages inside "pkgs" folder
 for pkg in os.listdir("pkgs"):
     print("\n=========================================\n")
@@ -14,49 +18,69 @@ for pkg in os.listdir("pkgs"):
     with open("pkgs/" + pkg, "r") as pkg_file:
         # Load package data
         pkg_data = json.load(pkg_file)
-        
-        download_url = pkg_data["download_url"]
 
         print("CHECKING " + pkg_name + "...")
-        try:
-            print(" DOWNLOAD: ...", end=' ')
-            # clone the package into ".temp" folder
-            os.system("git clone " + download_url + " .temp --quiet --depth 1")
-            print("OK")
-
-            # See if the package has a "sn.toml" file
-            print(" PACKAGE CONFIG: ...", end=' ')
-            if os.path.isfile(".temp/sn.toml"):
-                # If it does, then it's a valid package
-                print("OK")
-
-                # Load the "sn.toml" file
-                with open(".temp/sn.toml", "r") as sn_toml_file:
-                    # Load the "sn.toml" file
-                    sn_toml_data = tomllib.loads(sn_toml_file.read())
-
-                    print(" ENTRY EXISTENCE: ...", end=' ')
-                    # See if the package has an entry
-                    if "main" in sn_toml_data["package"]:
-                        entry = sn_toml_data["package"]["main"]
-                        with open(f".temp/{entry}", "r") as entry_file:
-                            print("OK")
-                    else:
-                        # If it doesn't, then it's an invalid package
-                        print("INVALID")
-                        has_error = True
-            else:
-                # If it doesn't, then it's an invalid package
-                print("INVALID")
-                has_error = True
-            
-        except Exception as e:
-            print("ERROR")
-            print(e)
+        if "versions" not in pkg_data:
+            print(RED + "INVALID (no verion tree)" + RESET)
             has_error = True
+            continue
 
-        # Delete the ".temp" folder
-        os.system("rm -rf .temp")
+        versions = pkg_data["versions"]
+        for version in versions:
+            download_url = pkg_data["download_url"]
+            print(" VERSION " + GREEN + version + RESET + ":")
+            try:
+                print("    DOWNLOAD: ...", end=' ')
+                # clone the package into ".temp" folder
+                os.system("git clone " + download_url + " .temp --quiet --depth 1 --branch " + version)
+                print(GREEN + "OK" + RESET)
+
+                # See if the package has a "sn.toml" file
+                print("    PACKAGE CONFIG: ...", end=' ')
+                if os.path.isfile(".temp/sn.toml"):
+                    # If it does, then it's a valid package
+                    print(GREEN + "OK" + RESET)
+
+                    # Load the "sn.toml" file
+                    with open(".temp/sn.toml", "r") as sn_toml_file:
+                        # Load the "sn.toml" file
+                        sn_toml_data = tomllib.loads(sn_toml_file.read())
+
+                        print("    ENTRY EXISTENCE: ...", end=' ')
+                        # See if the package has an entry
+                        if "main" in sn_toml_data["package"]:
+                            entry = sn_toml_data["package"]["main"]
+                            with open(f".temp/{entry}", "r") as entry_file:
+                                print(GREEN + "OK" + RESET)
+                        else:
+                            # If it doesn't, then it's an invalid package
+                            print(RED + "INVALID" + RESET)
+                            has_error = True
+
+                        print("    DEPENDENCIES EXISTENCE: ...", end=' ')
+                        # See if the package has dependencies
+                        if "dependencies" in sn_toml_data:
+                            dependencies = sn_toml_data["dependencies"]
+                            for dependency in dependencies:
+                                # only check if the dependency exists in the pkgs folder
+                                if os.path.isfile(f"pkgs/{dependency}.json"):
+                                    print(GREEN + "OK" + RESET)
+                                else:
+                                    print(RED + "INVALID" + RESET)
+                                    has_error = True
+                        else: print(GREEN + "OK" + RESET)
+                else:
+                    # If it doesn't, then it's an invalid package
+                    print(RESET + "INVALID" + RESET)
+                    has_error = True
+                
+            except Exception as e:
+                print(RED + "ERROR" + RESET)
+                print(e)
+                has_error = True
+
+            # Delete the ".temp" folder
+            os.system("rm -rf .temp")
 
 if has_error:
     exit(1)
--- check_packages.sh
+++ check_packages.sh
@@ -3,5 +3,8 @@ sudo apt-get update
 sudo apt-get purge python3 -y --auto-remove
 sudo apt-get install python3.11 -y
 
+# git disable advice.detachedHead
+git config --global advice.detachedHead false
+
 python3.11 -m pip install --upgrade pip
 python3.11 check_packages.py
