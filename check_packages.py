1b7df0b7593ed52865b2ce009a7d7b4da89c3e6a
mauro 🤙 <mauro.balades@tutanota.com>
2024-05-24 15:04:41 +0000

    Added std library

--- check_packages.py
+++ check_packages.py
@@ -1,7 +1,6 @@
 
 import os
 import json
-import tomllib
 
 has_error = False
 
@@ -36,39 +35,39 @@ for pkg in os.listdir("pkgs"):
                 print(GREEN + "OK" + RESET)
 
                 # See if the package has a "sn.toml" file
-                print("    PACKAGE CONFIG: ...", end=' ')
-                if os.path.isfile(".temp/sn.toml"):
+                print("    PACKAGE DEPS: ...", end=' ')
+                if os.path.isfile(".temp/deps.reky"):
                     # If it does, then it's a valid package
                     print(GREEN + "OK" + RESET)
-
-                    # Load the "sn.toml" file
-                    with open(".temp/sn.toml", "r") as sn_toml_file:
-                        # Load the "sn.toml" file
-                        sn_toml_data = tomllib.loads(sn_toml_file.read())
-
-                        print("    ENTRY EXISTENCE: ...", end=' ')
-                        # See if the package has an entry
-                        if "main" in sn_toml_data["package"]:
-                            entry = sn_toml_data["package"]["main"]
-                            with open(f".temp/{entry}", "r") as entry_file:
-                                print(GREEN + "OK" + RESET)
-                        else:
-                            # If it doesn't, then it's an invalid package
+                    content = open(".temp/deps.reky", "r").read()
+                    for dep in content.splitlines():
+                        if dep.startswith("#"):
+                            continue
+                        line = dep.strip()
+                        if line == "":
+                            continue
+                        split = line.split("==")
+                        if len(split) != 2:
                             print(RED + "INVALID" + RESET)
                             has_error = True
-
-                        print("    DEPENDENCIES EXISTENCE: ...", end=' ')
-                        # See if the package has dependencies
-                        if "dependencies" in sn_toml_data:
-                            dependencies = sn_toml_data["dependencies"]
-                            for dependency in dependencies:
-                                # only check if the dependency exists in the pkgs folder
-                                if os.path.isfile(f"pkgs/{dependency}.json"):
+                            break
+                        dep_name = split[0]
+                        dep_version = split[1]
+                        print("        " + dep_name + " " + dep_version + ": ...", end=' ')
+                        try:
+                            # Check if the dependency exists
+                            with open("pkgs/" + dep_name + ".json", "r") as dep_file:
+                                dep_data = json.load(dep_file)
+                                if dep_version in dep_data["versions"]:
                                     print(GREEN + "OK" + RESET)
                                 else:
                                     print(RED + "INVALID" + RESET)
                                     has_error = True
-                        else: print(GREEN + "OK" + RESET)
+                        except Exception as e:
+                            print(RED + "ERROR" + RESET)
+                            print(e)
+                            has_error = True
+
                 else:
                     # If it doesn't, then it's an invalid package
                     print(RESET + "INVALID" + RESET)
